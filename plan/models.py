a4bfd8d1b11344a455c245109198f6808e047405
WBSFIREBLADE <145446109+WBSFIREBLADE@users.noreply.github.com>
2024-10-07 14:38:34 +0530

    Add Books API with models, serializers, and views

--- plan/models.py
+++ plan/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Book(models.Model):
+    book_name = models.CharField(max_length=100)
+    book_price = models.IntegerField()
+    book_aurthor = models.CharField(max_length=50)
+    book_drscription = models.TextField()
+
+    
+
 # Create your models here.
--- plan/views.py
+++ plan/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .serializers import BookSerializers
+from .models import Book
+from rest_framework import viewsets
+
+
+class BookViewset(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializers
 
 # Create your views here.
